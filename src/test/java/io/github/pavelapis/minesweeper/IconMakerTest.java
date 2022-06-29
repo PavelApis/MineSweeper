86cbdcc4176e678a74643c43c1a913d5d9793b95
PavelApis <papchelnikov@gmail.com>
2022-06-29 11:57:39 +0300

    add files test

--- src/test/java/io/github/pavelapis/minesweeper/IconMakerTest.java
+++ src/test/java/io/github/pavelapis/minesweeper/IconMakerTest.java
@@ -2,9 +2,22 @@ package io.github.pavelapis.minesweeper;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
 public class IconMakerTest {
+    private static Path pathToSprites = FileSystems.getDefault().getPath("src", "main", "java", "sprites");
+    /*
+        Files in resources directory "src/main/java/sprites" should exist.
+     */
     @Test
     void pathsToSprites(){
-
+        for(int i = 1; i < 10; i++){
+            assertThat(pathToSprites.resolve(i + ".jpg").toFile().exists()).isTrue();
+        }
+        assertThat(pathToSprites.resolve("mine.jpg").toFile().exists()).isTrue();
     }
 }
