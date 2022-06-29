4b20ccc3de80fbbac108e75fa547ff8e1aafac66
PavelApis <papchelnikov@gmail.com>
2022-06-29 12:14:22 +0300

    fix files test

--- src/test/java/io/github/pavelapis/minesweeper/IconMakerTest.java
+++ src/test/java/io/github/pavelapis/minesweeper/IconMakerTest.java
@@ -9,15 +9,14 @@ import java.nio.file.Path;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class IconMakerTest {
-    private static Path pathToSprites = FileSystems.getDefault().getPath("src", "main", "java", "sprites");
     /*
         Files in resources directory "src/main/java/sprites" should exist.
      */
     @Test
     void pathsToSprites(){
-        for(int i = 1; i < 10; i++){
-            assertThat(pathToSprites.resolve(i + ".jpg").toFile().exists()).isTrue();
+        for(int i = 1; i < 9; i++){
+            assertThat(Thread.currentThread().getContextClassLoader().getResourceAsStream(i + ".jpg")).isNotNull();
         }
-        assertThat(pathToSprites.resolve("mine.jpg").toFile().exists()).isTrue();
+        assertThat(Thread.currentThread().getContextClassLoader().getResourceAsStream("mine.jpg")).isNotNull();
     }
 }
